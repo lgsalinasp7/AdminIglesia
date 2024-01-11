875fc2b02fe33675930bda786c6edffd4ab594b5
lgsalinasp7 <lgsalinasp@gmail.com>
2024-01-11 16:11:01 -0500

    Update classic component and navigation data

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,12 +3,70 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
+        id   : 'Inicio',
         title: 'Example',
         type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:home',
         link : '/example'
-    }
+    },
+    {
+        id   : 'Inicio',
+        title: 'Inicio',
+        type : 'basic',
+        icon : 'heroicons_outline:home',
+        link : '/home'
+    },
+    {
+        id   : 'personas',
+        title: 'Personas',
+        type : 'basic',
+        icon : 'heroicons_outline:user',
+        link : '/example'
+    },
+    {
+        id   : 'academia',
+        title: 'Academia',
+        type : 'basic',
+        icon : 'heroicons_outline:academic-cap',
+        link : '/example'
+    },
+    {
+        id   : 'peticiones',
+        title: 'Peticiones',
+        type : 'basic',
+        icon : 'heroicons_outline:document-text',
+        link : '/example'
+    },
+    {
+        id   : 'grupos',
+        title: 'Grupos',
+        type : 'basic',
+        icon : 'heroicons_outline:user-group',
+        link : '/example'
+    },
+    {
+        id   : 'actividades',
+        title: 'Actividades',
+        type : 'basic',
+        icon : 'heroicons_outline:clipboard',
+        link : '/example'
+    },
+    {
+        id   : 'ingresos',
+        title: 'Ingresos',
+        type : 'basic',
+        icon : 'heroicons_outline:calculator',
+        link : '/example'
+    },
+    {
+        id   : 'sesion',
+        title: 'Cerrar sesión',
+        type : 'basic',
+        icon : 'login',
+        link : '/example'
+    },
+  
+  
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
@@ -24,7 +82,7 @@ export const futuristicNavigation: FuseNavigationItem[] = [
         id   : 'example',
         title: 'Example',
         type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:home',
         link : '/example'
     }
 ];
